fe74029daffc0d1a3007db1f4857185d667ddd3b
Adam Joly <adjoly@2E2.42angouleme.fr>
2023-12-14 08:52:47 +0100

    get it from gh second try

--- styling.js
+++ styling.js
@@ -3,12 +3,8 @@ if (document.body.textContent.includes('Seekers')) {
 	console.log('✅ he is a gigachad');
 	var cssId = 'seekersdiff';
 	var head  = document.getElementsByTagName('head')[0];
-	var link  = document.createElement('link');
-	link.id   = cssId;
-	link.rel  = 'stylesheet';
-	link.type = 'text/css';
-	link.href = "https://raw.githubusercontent.com/KeyZox71/seekersrealcolor/main/styling.js";
-	link.media = 'all';
+	var link  = document.createElement('style');
+	link.link = "https://raw.githubusercontent.com/KeyZox71/seekersrealcolor/main/styles.css";
 	head.appendChild(link);
   } else {
 	console.log('⛔️ booooo loosers');
