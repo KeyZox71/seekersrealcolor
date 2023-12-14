d2bcfe4c8f33d569814f6b0c60e6b3504a55c583
Adam Joly <adjoly@2E2.42angouleme.fr>
2023-12-14 10:16:06 +0100

    fix coal rank page

--- styling.js
+++ styling.js
@@ -1,7 +1,7 @@
 // Check if Seekers
 if (document.body.textContent.includes('Seekers')) {
 	console.log('✅ he is a gigachad');
-	var css = '.coalition-flag--flag { \n	fill: #D950FF !important;\n}\n.coalition-span {\n	color: #D950FF !important;\n}\n#metrics  li {\n	background-color: #d950ff !important;\n}\n#metrics ol {\n	border-left: 1px solid rgb(217, 50, 255) !important;\n}\n.profile-banner {\n	background-image: url("https://raw.githubusercontent.com/KeyZox71/seekersrealcolor/main/seekers-login.png") !important;\n}\n.progress-bar {\n	background: #d950ff !important;\n}';
+	var css = '.coalition-flag--flag { \n	fill: #D950FF !important;\n}\n.coalition-span {\n	color: #D950FF !important;\n}\n#metrics  li {\n	background-color: #d950ff !important;\n}\n#metrics ol {\n	border-left: 1px solid rgb(217, 50, 255) !important;\n}\n.profile-banner {\n	background-image: url("https://raw.githubusercontent.com/KeyZox71/seekersrealcolor/main/seekers-login.png") !important;\n}\n.progress-bar {\n	background: #d950ff !important;\n}\ndiv#coalition-top-body a {\n	color: #d950ff !important;\n}\ndiv#coalition-global-data a {\n    color: #d950ff !important;\n}\ndiv#coalition-cover {\n	background-image: url("https://raw.githubusercontent.com/KeyZox71/seekersrealcolor/main/seekers-login.png") !important;\n}';
     head = document.head || document.getElementsByTagName('head')[0],
     style = document.createElement('style');
 
