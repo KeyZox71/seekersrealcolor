8e366902a172c92bb1b80a80fa128196eab1e131
Adam Joly <adjoly@2E2.42angouleme.fr>
2023-12-14 09:23:40 +0100

    it work for chrome

--- styling.js
+++ styling.js
@@ -1,11 +1,19 @@
 // Check if Seekers
 if (document.body.textContent.includes('Seekers')) {
 	console.log('✅ he is a gigachad');
-	var cssId = 'seekersdiff';
-	var head  = document.getElementsByTagName('head')[0];
-	var link  = document.createElement('style');
-	link.link = "https://raw.githubusercontent.com/KeyZox71/seekersrealcolor/main/styles.css";
-	head.appendChild(link);
-  } else {
+	var css = '.coalition-flag--flag { \n	fill: #D950FF !important;\n}\n.coalition-span {\n	color: #D950FF !important;\n}\n#metrics  li {\n	background-color: #d950ff !important;\n}\n#metrics ol {\n	border-left: 1px solid rgb(217, 50, 255) !important;\n}\n.profile-banner {\n	background-image: url("https://raw.githubusercontent.com/KeyZox71/seekersrealcolor/main/seekers-login.png") !important;\n}\n.progress-bar {\n	background: #d950ff !important;\n}';
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+	head.appendChild(style);
+
+	style.type = 'text/css';
+	if (style.styleSheet){
+	  // This is required for IE8 and below.
+	  style.styleSheet.cssText = css;
+	} else {
+	  style.appendChild(document.createTextNode(css));
+	}
+} else {
 	console.log('⛔️ booooo loosers');
   }
